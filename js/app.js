b813dd02338c55d66c75e65153f7efe4ca6c4b28
Dan <ddd11chevy@gmail.com>
2015-07-16 13:52:03 -0700

    Add images and the button works.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 var array = [];
 
-var reply = prompt("Put in something.");
-
-array.push(reply);
+function todo() {
+	var reply = prompt("Put in something.");
+	array.push(reply);
+	var node = document.createElement("LI");                 // Create a <li> node
+	var textnode = document.createTextNode(reply);           // Create a text node
+	node.appendChild(textnode);                              // Append the text to <li>
+	document.getElementById("myList").appendChild(node);
+}

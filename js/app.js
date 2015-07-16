ea7eb9c17bc22f534a24f87eb92d8e02247bd453
Dan <ddd11chevy@gmail.com>
2015-07-16 13:59:24 -0700

    Edge Casing

--- js/app.js
+++ js/app.js
@@ -2,9 +2,14 @@ var array = [];
 
 function todo() {
 	var reply = prompt("Put in something.");
-	array.push(reply);
-	var node = document.createElement("LI");                 // Create a <li> node
-	var textnode = document.createTextNode(reply);           // Create a text node
-	node.appendChild(textnode);                              // Append the text to <li>
-	document.getElementById("myList").appendChild(node);
+	if (reply == ""){
+		alert("YOU DIDN'T PUT ANYTIHING IN!");
+		todo();
+	} else {
+		array.push(reply);
+		var node = document.createElement("LI");                 // Create a <li> node
+		var textnode = document.createTextNode(reply);           // Create a text node
+		node.appendChild(textnode);                              // Append the text to <li>
+		document.getElementById("myList").appendChild(node);
+	}
 }
